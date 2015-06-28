87535b3bf5e38b0d045e33da654454719126f997
Ray Booysen <ray@sumatro.co.uk>
2015-06-28 22:05:21 +0100

    Another small fix

--- Esp.Net/EventContext.cs
+++ Esp.Net/EventContext.cs
@@ -21,6 +21,7 @@ namespace Esp.Net
     public interface IEventContext
     {
         bool IsCanceled { get; }
+        bool IsCommitted { get; }
         void Cancel();
         void Commit();
     }
