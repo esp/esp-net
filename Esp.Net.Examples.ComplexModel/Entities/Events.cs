cab8bf782b1600c5aa8399c20b82f94ee8a35d94
KeithWoods <devshop7@gmail.com>
2015-08-04 21:04:55 +0100

    spiking some modeling designs

---
+++ Esp.Net.Examples.ComplexModel/Entities/Events.cs
@@ -0,0 +1,22 @@
+﻿namespace Esp.Net.Examples.ComplexModel.Entities
+{
+    public class NotionalChangedEvent
+    {
+        public NotionalChangedEvent(decimal? notional)
+        {
+            Notional = notional;
+        }
+
+        public decimal? Notional { get; private set; } 
+    }
+
+    public class CurrencyPairChangedEvent
+    {
+        public CurrencyPairChangedEvent(string currencyPair)
+        {
+            CurrencyPair = currencyPair;
+        }
+
+        public string CurrencyPair { get; private set; }
+    }
+}
