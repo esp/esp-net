30f7199655f9e015700424b40a019a0a08f98f02
KeithWoods <devshop7@gmail.com>
2015-08-20 19:34:03 +0100

    #34 done: Adding support to create a scoped IRouter<T> that only targets a subset of another model

--- src/Esp.Net.Tests/RouterTests.cs
+++ src/Esp.Net.Tests/RouterTests.cs
@@ -806,7 +806,7 @@ namespace Esp.Net
                 }
             }
         }
-
+        
         public class ModelRouter : RouterTests
         {
             private IRouter<TestModel> _modelRouter;
@@ -843,11 +843,44 @@ namespace Esp.Net
                 _model1EventProcessor.Event1Details.NormalStage.ReceivedEvents.Count.ShouldBe(1);
                 _model2EventProcessor.Event1Details.NormalStage.ReceivedEvents.Count.ShouldBe(1);
             }
+        }
+
+        public class ModelSubRouter : RouterTests
+        {
+            private IRouter<SubTestModel> _modelRouter;
+
+            [SetUp]
+            public override void SetUp()
+            {
+                base.SetUp();
+                _modelRouter = _router.CreateModelRouter<TestModel, SubTestModel>(_model1.Id, m => m.SubTestModel);
+            }
 
             [Test]
-            public void CanExeuteProxiedEvent()
+            public void CanPublishAndObserveProxiedEvent()
             {
-                Assert.Inconclusive();
+                List<Tuple<SubTestModel, Event1>> receivedSubModels = new List<Tuple<SubTestModel, Event1>>();
+                _modelRouter.GetEventObservable<Event1>().Observe((m, e, c) => receivedSubModels.Add(Tuple.Create(m, e)));
+                _modelRouter.PublishEvent(new Event1());
+                receivedSubModels.Count.ShouldBe(1);
+                receivedSubModels[0].Item1.ShouldBe(_model1.SubTestModel);
+            }
+
+            [Test]
+            public void CanObserveProxiedModel()
+            {
+                var receivedModelCount = 0;
+                _modelRouter.GetModelObservable().Observe(m => receivedModelCount++);
+                _modelRouter.PublishEvent(new Event1());
+                receivedModelCount.ShouldBe(1);
+            }
+
+            [Test]
+            public void CanBroadcastProxiedEvent()
+            {
+                _modelRouter.BroadcastEvent(new Event1());
+                _model1EventProcessor.Event1Details.NormalStage.ReceivedEvents.Count.ShouldBe(1);
+                _model2EventProcessor.Event1Details.NormalStage.ReceivedEvents.Count.ShouldBe(1);
             }
         }
 
@@ -1064,9 +1097,16 @@ namespace Esp.Net
             public TestModel()
             {
                 Id = Guid.NewGuid();
+                SubTestModel = new SubTestModel();
             }
             public Guid Id { get; private set; }
             public bool ControllerShouldRemove { get; set; }
+            public SubTestModel SubTestModel { get; private set; }
+        }
+
+        public class SubTestModel
+        {
+
         }
 
         public class TestModel3
