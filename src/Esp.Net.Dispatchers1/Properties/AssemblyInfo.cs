ed9eefe6adf7d570480145b1c96a1be776352890
KeithWoods <devshop7@gmail.com>
2015-09-04 21:22:23 +0100

    Issues #43: threading work. Adding a dispatchers project and nuget package.

--- src/Esp.Net.Dispatchers1/Properties/AssemblyInfo.cs
+++ src/Esp.Net.Dispatchers1/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Esp.Net.PlatformServices")]
+[assembly: AssemblyTitle("Esp.Net.Dispatchers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Esp.Net.PlatformServices")]
+[assembly: AssemblyProduct("Esp.Net.Dispatchers")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("32e4c06b-d278-4595-bf98-c83e0040715a")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
