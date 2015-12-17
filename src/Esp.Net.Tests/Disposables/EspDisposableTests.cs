618074076c710d6119fc7eaf3dbbb76b37b01eaf
Ray Booysen <ray@sumatro.co.uk>
2015-12-17 22:07:31 +0000

    Proper copyright

--- src/Esp.Net.Tests/Disposables/EspDisposableTests.cs
+++ src/Esp.Net.Tests/Disposables/EspDisposableTests.cs
@@ -1,5 +1,5 @@
 ﻿#region copyright
-// Copyright 2015 Keith Woods
+// Copyright 2015 Dev Shop Limited
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
